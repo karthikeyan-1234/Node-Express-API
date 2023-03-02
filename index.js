7bdf8c4a69803dd40a2bbc6174ff185fac34275f
karthikeyan-1234 <87603111+karthikeyan-1234@users.noreply.github.com>
2023-03-03 03:17:26 +0530

    Added Redis service module

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const users = require("./modules/users");
+const redis = require("./modules/redisModule");
 
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 app.use(bodyParser.json());
 app.use(cors());
 
+
 app.use("",users);
+
+app.get("/addkey",(req,res) => {
+    redis.SET('foo','bar');
+    res.send(200,"Redis SET");
+})
+
+app.get("/getkey",(req,res) => {
+    redis.GET('foo').then((ret) => {
+        res.send(200,ret);
+    })
+})
+
 
 app.listen(PORT,() => {
     console.log(`Server is running on PORT: http://localhost:${PORT}`)
